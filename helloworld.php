07569250053968cadf252d92785e97f1ff04378d
JooBahx <dmattson2011@hotmail.com>
2016-09-16 22:20:44 -0400

    Hello World
    New login

--- helloworld.php
+++ helloworld.php
@@ -1,3 +1,23 @@
-<p 
-echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title></title>
+    </head>
+    <body>
+        <form action = "<?php $_PHP_SELF ?>" method = "GET">
+            Email: <input type = "text" name = "Email" />
+            Password: <input type = "text" name = "Password" />
+            <input type = "submit" />
+        </form>
+        
+        <?php
+            echo "Megan, My first website!<br />";
+
+            if( $_GET["Email"] || $_GET["Password"] ) {
+                echo "Welcome ". $_GET['Email']. "<br />";
+                exit();
+            }
+        ?>  
+    </body>
+</html>
