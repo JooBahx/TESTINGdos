a712176788456e015f1a5e86deaa52d42e6f8cbf
JooBahx <dmattson2011@hotmail.com>
2016-09-17 16:29:13 -0400

    mySQL comms

--- helloworld.php
+++ helloworld.php
@@ -1,4 +1,4 @@
-!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <title></title>
@@ -12,27 +12,36 @@
         
         <?php
             $servernamesql = "localhost";
-            $usernamesql = "uroot";
-            $passwordsql = "proot";
+            $usernamesql = "root";
+            $passwordsql = "password";
+            $email = "email";
 
             //Create connection
-            $conn = mysqli_connect($servernamesql, $usernamesql, $passwordsql);
-
-            // Check connection
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
+            $conn = mysql_connect($servernamesql, $usernamesql, $passwordsql);
+            
+            mysql_select_db($email,$conn);
+            $query = "SELECT * FROM $user";
+            $result = mysql_query($query);
+            
+            if($result) {
+                while($row = mysql_fetch_array($result)){
+                    $name = $row["$yourfield"];
+                    echo "Name: " . $name;  
+                }
             }
             
-            echo "<br /> Connected successfully";
+            
+            
+            echo "Connected successfully";
+            
+            mysql_close($conn);
+            echo "ASDF";
         
             if( $_GET["Email"] || $_GET["Password"] ) {
-               // echo "Welcome ". $_GET['Email']. "<br />";
+                echo "Welcome ". $_GET['Email']. "<br />";
                 exit();
-        
-                
             }
+            
         ?>  
     </body>
 </html>
-
-
